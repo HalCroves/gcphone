d3bacff12ac1023142434393796c20deabe9d6c1
HalCroves <ratatouilledu23@hotmail.fr>
2018-11-20 11:27:53 +0100

    Update bank.lua

--- gcphone/client/bank.lua
+++ gcphone/client/bank.lua
@@ -11,20 +11,20 @@
 
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(playerData)
-      local accounts = playerData.accounts or {}
-      for index, account in ipairs(accounts) do 
-            if account.name == 'bank' then
-                  SendNUIMessage({event = 'updateBankbalance', banking = account.money})
-                  break
-            end
-      end
+    local accounts = playerData.accounts or {}
+    for index, account in ipairs(accounts) do
+        if account.name == 'bank' then
+            SendNUIMessage({event = 'updateBankbalance', banking = account.money})
+            break
+        end
+    end
 end)
 
 RegisterNetEvent('esx:setAccountMoney')
 AddEventHandler('esx:setAccountMoney', function(account)
-      if account.name == 'bank' then
-            SendNUIMessage({event = 'updateBankbalance', banking = account.money})
-      end
+    if account.name == 'bank' then
+        SendNUIMessage({event = 'updateBankbalance', banking = account.money})
+    end
 end)
 
 
